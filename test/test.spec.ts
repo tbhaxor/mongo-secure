405444a5345d50d8a7478c751dea5e3a777a1e5f
Gurkirat Singh <tbhaxor@gmail.com>
2020-12-19 14:41:49 +0530

    fixed test suites

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 import { serializer } from '../'
 
-describe('MongoSecure Testing', () => {
+describe('MongoSecure Testing (with maxNestingLevel)', () => {
   let post: any
   const replacer = Math.random().toString(36).slice(2)
 
@@ -35,6 +35,56 @@ describe('MongoSecure Testing', () => {
   // eslint-disable-next-line quotes
   it("should set 'app2' to '" + replacer + "' and 'app' to 1 when limit 1", done => {
     const r: any = serializer(post, 1, { maxNestingLevel: 1, replaceWith: replacer })
+    expect(r.app2).to.be.equal(replacer)
+    expect(r.app).to.be.equal(post.app)
+    done()
+  })
+
+  // eslint-disable-next-line quotes
+  it("should set 'app2.app3' to '" + replacer + "' when limit 2", done => {
+    const r: any = serializer(post, 1, { maxNestingLevel: 2, replaceWith: replacer })
+    expect(r.app).to.be.equal(post.app)
+    expect(typeof r.app2).to.be.equal(typeof post.app2)
+    expect(r.app2.app2).to.be.equal(post.app2.app2)
+    expect(r.app2.app3).to.be.equal(replacer)
+    done()
+  })
+})
+
+describe('MongoSecure Testing (with limit)', () => {
+  let post: any
+  const replacer = Math.random().toString(36).slice(2)
+
+  beforeEach(done => {
+    post = { app: 1, app2: { app2: 1, app3: { app4: 1 } } }
+    done()
+  })
+
+  it('should set limit to 1 when it is undefined', done => {
+    // @ts-ignore
+    const r: any = serializer(post, 1, { limit: undefined, replaceWith: replacer })
+    expect(r.app).to.be.equal(post.app)
+    expect(r.app2).to.be.equal(replacer)
+    done()
+  })
+
+  it('should set limit to 1 when it is 0', done => {
+    const r: any = serializer(post, 1, { limit: 0, replaceWith: replacer })
+    expect(r.app).to.be.equal(post.app)
+    expect(r.app2).to.be.equal(replacer)
+    done()
+  })
+
+  // eslint-disable-next-line quotes
+  it("should send '" + replacer + "' as the replaced content", done => {
+    const r: any = serializer(post, 1, { limit: 0, replaceWith: replacer })
+    expect(r.app2).to.be.equal(replacer)
+    done()
+  })
+
+  // eslint-disable-next-line quotes
+  it("should set 'app2' to '" + replacer + "' and 'app' to 1 when limit 1", done => {
+    const r: any = serializer(post, 1, { limit: 1, replaceWith: replacer })
     expect(r.app2).to.be.equal(replacer)
     expect(r.app).to.be.equal(post.app)
     done()
